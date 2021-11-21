85aedd2c63559648b11a68c362479f88c36a9eba
torvic <130111@unsaac.edu.pe>
2021-11-21 17:06:17 -0500

    App finished

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,49 @@
-const {Router} = require('express')
+const { Router } = require('express')
 const router = Router()
 
 const Photo = require('../models/Photo')
-
-router.get('/', (req, res) => {
-	res.render('partials/images')
+const cloudinary = require('cloudinary')
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
-router.get('/images/add', (req, res) => {
-	res.render('partials/image_form')
+const fs = require('fs-extra')
+
+router.get('/', async (req, res) => {
+	const photos = await Photo.find().lean()
+	res.render('partials/images', { photos })
 })
 
-router.post('/images/add', (req, res) => {
+router.get('/images/add', async (req, res) => {
+	const photos = await Photo.find().lean()
+	res.render('partials/image_form', { photos })
+})
+
+router.post('/images/add', async (req, res) => {
 	console.log(req.body);
 	console.log(req.file);
+	const result = await cloudinary.v2.uploader.upload(req.file.path)
+	console.log(result);
 	const { title, description } = req.body
-	new Photo({
+	const newPhoto = new Photo({
 		title,
 		description,
-		imageURL,
-		public_id
+		imageURL: result.url,
+		public_id: result.public_id
 	})
-	res.send('Received')
+	await newPhoto.save()
+	await fs.unlink(req.file.path)
+	res.redirect('/')
+})
+
+router.get('/images/delete/:photo_id', async (req, res) => {
+	const { photo_id } = req.params
+	const photo = await Photo.findByIdAndDelete(photo_id)
+	const result = await cloudinary.v2.uploader.destroy(photo.public_id)
+	console.log(result);
+	res.redirect('/images/add')
 })
 
 
